a93d704c17a81941b7b97c688f1b1d8500c62efa
Randy Westlund <rwestlun@gmail.com>
2016-09-03 03:19:45 -0400

    Add comments, reorder functions

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ use std::process::Command;
 use std::io::prelude::*;
 use std::fs::File;
 
+// This describes a process that we're responsible for running. We'll allocate
+// one for each one them.
 #[derive(Debug)]
 struct Process {
+    // A user-defined name of the process.
     name: String,
+    // The path to the executable, e.g. /bin/ls.
     path: String,
+    // A list of arguments to pass, e.g. [ "-a", "-l" ] or [].
     args: Vec<String>,
 }
 
@@ -21,15 +26,27 @@ fn main () {
 
     // Create a list of processes to manage.
     let mut processes : Vec<Process> = Vec::new();
-
+    // Fill the list of processes from the config file.
     parse_config_file(CONF_FILENAME, &mut processes);
 
     for p in processes {
         // Launch the program.
-        launch(&p);
+        run(&p);
     }
 }
 
+// Launch a program, given the program name and arguments.
+fn run(p: &Process) {
+    println!("starting process {:?}", p);
+    let child = Command::new(&p.path)
+        .args(&p.args)
+        .spawn()
+        .expect("failed to run binary");
+    println!("returned from spawning {}", p.name);
+}
+
+// Parse the config file (first arg), putting processes into the process list
+// (second argument).
 fn parse_config_file(filename: &str, processes: &mut Vec<Process>) {
     // Open the config file.
     let mut f = File::open(filename)
@@ -50,17 +67,18 @@ fn parse_config_file(filename: &str, processes: &mut Vec<Process>) {
     // Iterate over all defined processes.
     let procs = data.as_table().unwrap();
     for a in procs.keys() {
+        // Get a handle to the process Table.
         let def = procs.get(a)
             .unwrap()
             .as_table()
             .unwrap();
-        // Get values for 'path' and 'arg'.
         let path = def.get("path")
             .unwrap()
             .as_str()
             .unwrap();
-    // Split the args to prevent the whole string from being delivered as one
-    // argument.
+        // Getting arguments is more complicated, both because it's optional,
+        // and because it needs to be split to prevent the whole string from
+        // being delivered as one argument.
         let args = def.get("args")
             .and_then(|x| x.as_str())
             .and_then(|x| Some(
@@ -68,24 +86,11 @@ fn parse_config_file(filename: &str, processes: &mut Vec<Process>) {
                     .map(|x| x.to_string())
                     .collect()))
             .unwrap_or(Vec::new());
+        // Now that we have all the info, add it to the list.
         processes.push(Process {
             name: a.to_string(),
             path: path.to_string(),
             args: args
         });
     }
-}
-
-// Launch a program, given the program name and arguments.
-fn launch(p: &Process) {
-    println!("starting process {:?}", p);
-    let result = Command::new(&p.path)
-        .args(&p.args)
-        .spawn()
-        //.output()
-        .expect("failed to run binary");
-    println!("returned from spawning {}", p.name);
-    //println!("status {}", result.status);
-    //println!("stdout {}", String::from_utf8_lossy(&result.stdout));
-    //println!("stderr {}", String::from_utf8_lossy(&result.stderr));
 }
