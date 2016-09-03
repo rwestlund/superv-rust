392edf4c469d3f1dbe4d23bcc1d0782a096041c0
Randy Westlund <rwestlun@gmail.com>
2016-09-03 04:08:05 -0400

    Move the spawning of processes into new threads

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::str;
 use std::process::Command;
 use std::io::prelude::*;
 use std::fs::File;
+use std::thread;
+use std::sync::Arc;
 
 // This describes a process that we're responsible for running. We'll allocate
 // one for each one them.
@@ -16,6 +18,8 @@ struct Process {
     path: String,
     // A list of arguments to pass, e.g. [ "-a", "-l" ] or [].
     args: Vec<String>,
+    // The time to sleep between restarts, in milliseconds. Defaults to 0.
+    restart_delay: u64,
 }
 
 fn main () {
@@ -25,29 +29,41 @@ fn main () {
     //let progname = args[0].clone();
 
     // Create a list of processes to manage.
-    let mut processes : Vec<Process> = Vec::new();
+    let mut processes : Vec<Arc<Process>> = Vec::new();
     // Fill the list of processes from the config file.
     parse_config_file(CONF_FILENAME, &mut processes);
 
     for p in processes {
         // Launch the program.
-        run(&p);
+        run(p.clone());
     }
+    thread::sleep(std::time::Duration::from_millis(10000));
 }
 
-// Launch a program, given the program name and arguments.
-fn run(p: &Process) {
-    println!("starting process {:?}", p);
-    let child = Command::new(&p.path)
+// Keep the given process running. This should be run in a separate thread.
+fn run (p: Arc<Process>) {
+    thread::spawn(move || {
+        loop {
+            let mut child = launch(&*p);
+            let status = child.wait().expect("Child wasn't running!");
+            println!("child {} exited with status {}", p.name, status);
+            thread::sleep(std::time::Duration::from_millis(p.restart_delay));
+        }
+    });
+}
+
+// Launch a program, given the Process struct, then return the Child.
+fn launch(p: &Process) -> std::process::Child {
+    println!("starting process {}", p.name);
+    Command::new(&p.path)
         .args(&p.args)
         .spawn()
-        .expect("failed to run binary");
-    println!("returned from spawning {}", p.name);
+        .expect("failed to run binary")
 }
 
 // Parse the config file (first arg), putting processes into the process list
 // (second argument).
-fn parse_config_file(filename: &str, processes: &mut Vec<Process>) {
+fn parse_config_file(filename: &str, processes: &mut Vec<Arc<Process>>) {
     // Open the config file.
     let mut f = File::open(filename)
         .expect(&*format!("Configuration file {} not found.", filename));
@@ -76,6 +92,9 @@ fn parse_config_file(filename: &str, processes: &mut Vec<Process>) {
             .unwrap()
             .as_str()
             .unwrap();
+        let restart_delay = def.get("restart_delay")
+            .and_then(|x| x.as_integer())
+            .unwrap_or(0);
         // Getting arguments is more complicated, both because it's optional,
         // and because it needs to be split to prevent the whole string from
         // being delivered as one argument.
@@ -87,10 +106,11 @@ fn parse_config_file(filename: &str, processes: &mut Vec<Process>) {
                     .collect()))
             .unwrap_or(Vec::new());
         // Now that we have all the info, add it to the list.
-        processes.push(Process {
-            name: a.to_string(),
-            path: path.to_string(),
-            args: args
-        });
+        processes.push(Arc::new(Process {
+            name:           a.to_string(),
+            path:           path.to_string(),
+            args:           args,
+            restart_delay:  restart_delay as u64,
+        }));
     }
 }
